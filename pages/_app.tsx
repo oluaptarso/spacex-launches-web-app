1451507fbc4897295a4587d6aef61f3fab419a2f
oluaptarso <oluaptarsso@gmail.com>
2023-03-20 23:37:39 -0300

    feat(font): configure Roboto font on the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+
+const customNextFont = Roboto({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --custom-next-font: ${customNextFont.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
